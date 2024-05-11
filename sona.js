8ad61c18e17aa9dfcc415ca7f6d98943817655ed
Sonalyjain01 <142651702+Sonalyjain01@users.noreply.github.com>
2024-05-11 17:19:11 +0530

    Add files via upload

---
+++ sona.js
@@ -0,0 +1,60 @@
+console.log("this is my wesite");
+// Function in Java Script
+/*
+// wish=" wishing all the employees ( default value of wish)"
+function student (name, wish=" wishing all the employees"){
+    // local variables (name2="Amiy";)
+    let name2="Amiy";
+    console.log(name + " is an engineer");
+    console.log(wish +""+ name);
+    
+}
+let name="Sonaly";
+let name1="Amitesh";
+let name2="Parth";
+let name3="Aditya";
+let name4="Shudi";
+let wish="Good Morning , you are selected for this job ";
+student(name,wish);
+student(name1,wish);
+student(name2,wish);
+student(name3,wish);
+student(name4,wish);
+student(name);
+// return undefined value
+//let returnVal=student(name3);
+//console.log(returnVal);
+//console.log(name + " is an engineer");
+//console.log(name1 + " is an engineer");
+//console.log(name2 + " is an engineer");
+//console.log(name3 + " is an engineer");
+//console.log(name4 + " is an engineer");
+*/
+/*
+// sum of three numbers
+function sum(a,b,c){
+    let d=a +b+ c;
+    return d;
+    // This line will never be execute ( unreachable code)
+    //console.log("Function is return type");
+}
+let returnVal=sum(4,5,8);
+console.log(returnVal);
+*/
+// find which one greater, minimum or equal
+function number(a,b,c){
+    if (a>b){
+        return a;
+    }
+    else if ( b>c){
+        return b;
+    }
+    //else if (c=b=a) {
+        //return d;
+    //}
+    else{
+        return c
+    }
+}
+let returnVl=number(8,28,8);
+console.log(returnVl);
